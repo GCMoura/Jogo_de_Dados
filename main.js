416bd3d759f21562f457be00d062493d6e3436f9
Gabriel Moura <gabrielmoura29@gmail.com>
2019-09-19 17:29:58 -0300

    falta o resultado

--- main.js
+++ main.js
@@ -6,19 +6,82 @@ var dicewidth = 100; // largura do dado
 var diceheight = 100; // altura do dado
 var dotrad = 6; // o raio de um ponto
 var ctx; // o contexto da tela
+var dx; // posicionamento horizontal para cada uma das faces do dado
+var dy; // posicionamento vertical para cada uma das faces do dado
+
+var ponto1 = document.querySelector('#point1')
+var ponto2 = document.querySelector('#point2')
+var play1 = document.querySelector('#player1')
+var play2 = document.querySelector('#player2')
+var result = document.querySelector('#result')
+var soma = 0
+var lance = 0
+
+function iniciarJogo() {
+  player1 = prompt('Digite o nome do jogador 1: ')
+  player2 = prompt('Digite o nome do jogador 2: ')
+  play1.innerHTML = player1;
+  play2.innerHTML = player2;
+}
 
 function init() {
   var ch = 1 + Math.floor(Math.random() * 6); //define o valor de da face do dado entre 1 e 6
-  drawface(ch); //função para escrever a jogada do dad0
+  drawface(ch); //função para escrever a jogada do dado
+}
+
+function resultado(x, y) {
+
+    console.log(x,y)  
+  
+}
+
+function throwTwice() { //lançamento dos dois dados
+  let pl1 = '';
+  let pl2 = '';
+  var ch = 1 + Math.floor(Math.random() * 6); //define o valor de da face do dado entre 1 e 6
+  dx =  dicex;
+  dy = dicey;
+  drawface(ch);
+  soma = ch
+  dx = dicex + 180; //ajusta dx para a segunda face do dado, 
+  var ch = 1 + Math.floor(Math.random() * 6);
+  drawface(ch);
+  lance += 1;
+  console.log('Lance nº: ', lance)
+  soma += ch;
+  if (lance % 2 == 1){
+    pl1 = soma;
+    ponto1.innerHTML = soma;  
+  } else if (lance % 2 == 0) {
+    ponto2.innerHTML = soma;
+    pl2 = soma;
+    resultado(pl1, pl2)
+  }
+  
+}
+
+function emptyDice() { //Inicia o jogo com o dado vazio
+  ctx = document.getElementById("canvas").getContext("2d");
+  ctx.lineWidth = 5; // largura da linha é 5
+  ctx.clearRect(dx, dy, dicewidth, diceheight); // limpa a face do dado depois da primeira jogada
+  ctx.strokeRect(dx, dy, dicewidth, diceheight); // contorno da face do dado
+  ctx.fillStyle = "#009966";
+  newDx = dx - 180
+  ctx.clearRect(newDx, dy, dicewidth, diceheight); // limpa a face do dado depois da primeira jogada
+  ctx.strokeRect(newDx, dy, dicewidth, diceheight); // contorno da face do dado
+  pontos.innerHTML = ""; //zera os pontos
 }
 
 function drawface(n) {
   ctx = document.getElementById("canvas").getContext("2d");
   ctx.lineWidth = 5; // largura da linha é 5
-  ctx.clearRect(dicex, dicey, dicewidth, diceheight); // limpa a face do dado depois da primeira jogada
-  ctx.strokeRect(dicex, dicey, dicewidth, diceheight); // contorno da face do dado
+  ctx.clearRect(dx, dy, dicewidth, diceheight); // limpa a face do dado depois da primeira jogada
+  ctx.strokeRect(dx, dy, dicewidth, diceheight); // contorno da face do dado
+  var dotx;
+  var doty;
   ctx.fillStyle = "#009966";
   console.log(n);
+  
   switch (n) {
     case 1:
       draw1();
@@ -48,8 +111,8 @@ function draw1() {
   var dotx;
   var doty;
   ctx.beginPath();
-  dotx = dicex + 0.5 * dicewidth; //posicionamento horizontal do ponto do dado
-  doty = dicey + 0.5 * diceheight; // posicionamento vertical do ponto do dado
+  dotx = dx + 0.5 * dicewidth; //posicionamento horizontal do ponto do dado
+  doty = dy + 0.5 * diceheight; // posicionamento vertical do ponto do dado
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
   ctx.closePath();
   ctx.fill(); //círculo cheio = ponto do dado
@@ -59,11 +122,11 @@ function draw2() {
   var dotx;
   var doty;
   ctx.beginPath();
-  dotx = dicex + 3 * dotrad;
-  doty = dicey + 3 * dotrad;
+  dotx = dx + 3 * dotrad;
+  doty = dy + 3 * dotrad;
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
-  dotx = dicex + dicewidth - 3 * dotrad;
-  doty = dicey + diceheight - 3 * dotrad;
+  dotx = dx + dicewidth - 3 * dotrad;
+  doty = dy + diceheight - 3 * dotrad;
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
   ctx.closePath();
   ctx.fill(); //círculo cheio = ponto do dado
@@ -73,20 +136,20 @@ function draw4() {
   var dotx;
   var doty;
   ctx.beginPath();
-  dotx = dicex + 3 * dotrad;
-  doty = dicey + 3 * dotrad;
+  dotx = dx + 3 * dotrad;
+  doty = dy + 3 * dotrad;
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
-  dotx = dicex + dicewidth - 3 * dotrad;
-  doty = dicey + diceheight - 3 * dotrad;
+  dotx = dx + dicewidth - 3 * dotrad;
+  doty = dy + diceheight - 3 * dotrad;
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
   ctx.closePath();
   ctx.fill(); //círculo cheio = ponto do dado
   ctx.beginPath();
-  dotx = dicex + 3 * dotrad;
-  doty = dicey + diceheight - 3 * dotrad;
+  dotx = dx + 3 * dotrad;
+  doty = dy + diceheight - 3 * dotrad;
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
-  dotx = dicex + dicewidth - 3 * dotrad;
-  doty = dicey + 3 * dotrad;
+  dotx = dx + dicewidth - 3 * dotrad;
+  doty = dy + 3 * dotrad;
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
   ctx.closePath();
   ctx.fill(); //círculo cheio = ponto do dado
@@ -96,11 +159,11 @@ function draw2mid() {
   var dotx;
   var doty;
   ctx.beginPath();
-  dotx = dicex + 3 * dotrad;
-  doty = dicey + 0.5 * diceheight;
+  dotx = dx + 3 * dotrad;
+  doty = dy + 0.5 * diceheight;
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
-  dotx = dicex + dicewidth - 3 * dotrad;
-  doty = dicey + 0.5 * diceheight;
+  dotx = dx + dicewidth - 3 * dotrad;
+  doty = dy + 0.5 * diceheight;
   ctx.arc(dotx, doty, dotrad, 0, Math.PI * 2, true); //constroi o ponto do dado
   ctx.closePath();
   ctx.fill(); //círculo cheio = ponto do dado
