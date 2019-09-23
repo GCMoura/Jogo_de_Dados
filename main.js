df9be95b28dc7a6a1313fa7beec555fc1152c61c
Gabriel Moura <gabrielmoura29@gmail.com>
2019-09-22 22:16:49 -0300

    1

--- main.js
+++ main.js
@@ -9,65 +9,54 @@ var ctx; // o contexto da tela
 var dx; // posicionamento horizontal para cada uma das faces do dado
 var dy; // posicionamento vertical para cada uma das faces do dado
 
-var ponto1 = document.querySelector('#point1')
-var ponto2 = document.querySelector('#point2')
-var play1 = document.querySelector('#player1')
-var play2 = document.querySelector('#player2')
-var result = document.querySelector('#result')
-var soma = 0
-var lance = 0
-
-function iniciarJogo() {
-  player1 = prompt('Digite o nome do jogador 1: ')
-  player2 = prompt('Digite o nome do jogador 2: ')
-  play1.innerHTML = player1;
-  play2.innerHTML = player2;
-}
-
-function init() {
-  var ch = 1 + Math.floor(Math.random() * 6); //define o valor de da face do dado entre 1 e 6
-  drawface(ch); //função para escrever a jogada do dado
-}
+var ponto1 = document.querySelector("#point1");
+var ponto2 = document.querySelector("#point2");
+var play1 = document.querySelector("#player1");
+var play2 = document.querySelector("#player2");
+var result = document.querySelector("#result");
+var soma = 0;
+var lance = 0;
+var secreto = 0;
 
 function resultado(soma, lance) {
-  let resultadoFinal = [];
-  let pl1 = '';
-  let pl2 = '';
-  if (lance % 2 == 1){
-    pl1 = soma
+  if (lance % 2 == 1) {
     ponto1.innerHTML = soma;
   } else if (lance % 2 == 0) {
     ponto2.innerHTML = soma;
-    pl2 = soma
   }
-  resultadoFinal.push(pl1)
-  resultadoFinal.push(pl2)
-  console.log(resultadoFinal)
- 
 }
 
-function throwTwice() { //lançamento dos dois dados
+function throwTwice(secreto) {
+  //lançamento dos dois dados
   var ch = 1 + Math.floor(Math.random() * 6); //define o valor de da face do dado entre 1 e 6
-  dx =  dicex;
+  dx = dicex;
   dy = dicey;
   drawface(ch);
-  soma = ch
-  dx = dicex + 180; //ajusta dx para a segunda face do dado, 
+  soma = ch;
+  dx = dicex + 180; //ajusta dx para a segunda face do dado,
   var ch = 1 + Math.floor(Math.random() * 6);
   drawface(ch);
-  lance += 1;
-  console.log('Lance nº: ', lance)
   soma += ch;
+  lance += 1;
   resultado(soma, lance);
+  console.log("soma", soma);
+  console.log("secreto", secreto);
 }
 
-function emptyDice() { //Inicia o jogo com o dado vazio
+function emptyDice() {
+  //Inicia o jogo com o dado vazio
+  player1 = prompt("Digite o nome do jogador 1: ");
+  player2 = prompt("Digite o nome do jogador 2: ");
+  play1.innerHTML = player1;
+  play2.innerHTML = player2;
+  var secreto = 1 + Math.floor(Math.random() * 6);
+  console.log("Número secreto", secreto);
   ctx = document.getElementById("canvas").getContext("2d");
   ctx.lineWidth = 5; // largura da linha é 5
   ctx.clearRect(dx, dy, dicewidth, diceheight); // limpa a face do dado depois da primeira jogada
   ctx.strokeRect(dx, dy, dicewidth, diceheight); // contorno da face do dado
   ctx.fillStyle = "#009966";
-  newDx = dx - 180
+  newDx = dx - 180;
   ctx.clearRect(newDx, dy, dicewidth, diceheight); // limpa a face do dado depois da primeira jogada
   ctx.strokeRect(newDx, dy, dicewidth, diceheight); // contorno da face do dado
   ponto1.innerHTML = ""; //zera os pontos
@@ -83,7 +72,7 @@ function drawface(n) {
   var doty;
   ctx.fillStyle = "#009966";
   console.log(n);
-  
+
   switch (n) {
     case 1:
       draw1();
